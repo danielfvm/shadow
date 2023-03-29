092136822f9718d250e9efe3301b57aced6f10b7
DanielFvM <daniel@schloegl-software.at>
2023-03-29 14:26:01 +0200

    Added gui

--- gui/gui.py
+++ gui/gui.py
@@ -1,79 +1,186 @@
-#!/bin/bash
+import sys
 
-import tkinter
-from tkinter import ttk as tk
-from tkinter import filedialog
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 
-class PictureElement:
-    def __init__(self, image_path):
-        self.image_path = image_path
+from listelements import ListWidget
 
-class PictureListApp:
+from tkinter.filedialog import askopenfilename
+
+class WindowBar(QWidget):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        # Set the window bar properties
+        self.setFixedHeight(40)
+
+        self.setStyleSheet("""
+            QPushButton {
+                background-color: #2980b9;
+                border: none;
+                color: white;
+                padding: 25px 25px;
+                text-decoration: none;
+                display: inline-block;
+                font-size: 16px;
+                cursor: pointer;
+                margin: 5px 5px;
+                border-radius: 2px;
+                font-weight: 200;
+            }
+
+            QPushButton::hover {
+                background-color: #3498db;
+            }
+
+            QLabel {
+                background-color: rgba(0,0,0,0);
+                font-weight: 200;
+                color: white;
+                margin: 5px;
+            }
+        """)
+
+        # Create the title label and add it to the layout
+        self.title = QLabel("Show - Shader On Wallpaper")
+
+        # Create the close button and add it to the layout
+        self.close_btn = QPushButton("X")
+        self.close_btn.clicked.connect(self.parent().close)
+        self.close_btn.setFixedWidth(40)
+
+        # Create the layout for the window bar
+        layout = QHBoxLayout()
+        layout.addWidget(self.title)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.close_btn)
+
+        # Set the layout for the window bar
+        self.setLayout(layout)
+
+    def paintEvent(self, event):
+        # Override the paintEvent to draw a border around the window bar
+        painter = QPainter(self)
+        painter.fillRect(0, 0, self.width() - 1, self.height() - 1, QColor(20, 20, 20))
+
+    def mousePressEvent(self, event):
+        # Override the mousePressEvent to start the dragging process
+        if event.button() == Qt.LeftButton:
+            self.start_drag_pos = event.globalPos() - self.parent().pos()
+            event.accept()
+
+    def mouseMoveEvent(self, event):
+        # Override the mouseMoveEvent to move the window when dragging the window bar
+        if event.buttons() == Qt.LeftButton:
+            self.parent().move(event.globalPos() - self.start_drag_pos)
+            event.accept()
+
+
+
+class TabWidget(QWidget):
     def __init__(self):
-        self.picture_list = []
-        self.selected_index = -1
+        super().__init__()
 
-        # Create the UI
-        self.root = tkinter.tk.Tk()
-        self.root.title("Picture List App")
+        # Remove the window border and set the background color to white
+        self.setWindowFlag(Qt.FramelessWindowHint)
+        self.setStyleSheet("""
+            background-color: #303030;
+            font-size: 18px;
+            color: white;
+        """)
 
-        self.picture_list_frame = tk.Frame(self.root)
-        self.picture_list_frame.pack(side="left", fill="both", expand=True)
 
-        self.picture_list_box = tk.Listbox(self.picture_list_frame, selectmode="single")
-        self.picture_list_box.pack(side="left", fill="both", expand=True)
+        # Create a vertical layout for the Tab widget
+        layout = QVBoxLayout()
+        layout.setSpacing(0)
+        layout.setContentsMargins(0, 0, 0, 0)
 
-        self.picture_detail_frame = tk.Frame(self.root)
-        self.picture_detail_frame.pack(side="left", fill="both", expand=True)
+        # Create a custom window bar and add it to the layout
+        self.window_bar = WindowBar(self)
+        layout.addWidget(self.window_bar)
 
-        self.picture_detail_label = tk.Label(self.picture_detail_frame, text="No picture selected")
-        self.picture_detail_label.pack(pady=10)
+        # Create a Tab widget
+        self.tabs = QTabWidget(self)
 
-        self.apply_button = tk.Button(self.picture_detail_frame, text="Apply", command=self.apply_action)
-        self.apply_button.pack(pady=10)
+        # Set the background color of the Tab bar to gray and center the tabs
+        self.tabs.setStyleSheet("""
+            QTabWidget::tab-bar {
+                alignment: center;
+                border: none;
+            }
 
-        self.remove_button = tk.Button(self.picture_detail_frame, text="Remove", command=self.remove_picture)
-        self.remove_button.pack(pady=10)
+            QTabBar::tab {
+                background-color: #404040;
+                padding: 5px 50px 5px 50px;
+                margin: 5px 0px 5px 0px;
+                text-align: center;
+                border-radius: 2px;
+                font-weight: 200;
+            }
 
-        self.add_button = tk.Button(self.root, text="Add", command=self.add_picture)
-        self.add_button.pack(side="bottom", pady=10)
+            QTabBar::tab:selected {
+                background-color: #2980b9;
+            }
 
-    def add_picture(self):
-        # Open a file dialog to select a picture file
-        #file_path = filedialog.askopenfilename(title="Select a picture", filetypes=(("Image files", "*.jpg;*.jpeg;*.png")))
-        file_path = filedialog.askopenfilename(initialdir="/", title="Select An Image", filetypes=(("jpeg files", "*.jpg"), ("gif files", "*.gif*"), ("png files", "*.png")))
-        if file_path:
-            # Create a PictureElement instance with the selected picture file
-            picture_element = PictureElement(file_path)
-            self.picture_list.append(picture_element)
+            QTabBar::tab:pressed {
+                background-color: #2980b9;
+            }
 
-            # Add the picture element to the list box
-            self.picture_list_box.insert("end", file_path)
+            QTabWidget::pane {
+                border: 0
+            }
+        """)
 
-    def remove_picture(self):
-        if self.selected_index >= 0:
-            # Remove the selected picture element from the list and list box
-            del self.picture_list[self.selected_index]
-            self.picture_list_box.delete(self.selected_index)
+        # Add the tabs to the Tab widget
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tab3 = QWidget()
+        self.tabs.addTab(self.tab1, "Browse")
+        self.tabs.addTab(self.tab2, "Current")
+        self.tabs.addTab(self.tab3, "Settings")
+        self.tabs.setGeometry(100, 0, 200, 50)
 
-            # Clear the picture detail label
-            self.picture_detail_label.config(text="No picture selected")
-            self.selected_index = -1
+        #self.tab2.setStyleSheet("""
+        #    QPushButton {
+        #        background-color: #2980b9;
+        #        border: none;
+        #        color: white;
+        #        padding: 5px 25px;
+        #        text-decoration: none;
+        #        display: inline-block;
+        #        font-size: 16px;
+        #        cursor: pointer;
+        #        margin: 5px 5px;
+        #        border-radius: 2px;
+        #        font-weight: 200;
+        #    }
+        #""")
 
-    def apply_action(self):
-        if self.selected_index >= 0:
-            # Do some action with the selected picture element
-            picture_element = self.picture_list[self.selected_index]
-            # ...
-            pass
+        self.list_widget = ListWidget(self.tab2)
+        self.list_widget.setGeometry(0, 0, 600, 600)
+        self.list_widget.setDragDropMode(QAbstractItemView.InternalMove)
 
-    def run(self):
-        self.root.mainloop()
+        add_button = QPushButton('Import', self.tab2)
+        add_button.setGeometry(20, 650, 200, 50)
 
-def main():
-    print("test")
-    app = PictureListApp()
-    app.run()
+        add_button.clicked.connect(self.list_widget.add_item)
 
-if __name__ == "__main__":
-    main()
+        remove_button = QPushButton('Remove', self.tab2)
+        remove_button.clicked.connect(self.list_widget.remove_item)
+        remove_button.setGeometry(20 + 200 + 20, 650, 200, 50)
+
+
+        # Add the Tab widget to the layout
+        layout.addWidget(self.tabs)
+
+        # Set the layout for the widget
+        self.setLayout(layout)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    widget = TabWidget()
+    widget.setMinimumSize(600, 800)
+    widget.show()
+    sys.exit(app.exec_())
